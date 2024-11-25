27cf2d0945e0ae7506a7f3c6bfe62b5dbb59cca2
hiran_xdoto <hiran@xdoto.io>
2024-11-25 22:45:46 +0530

    Initial commit

---
+++ src/main/java/com/hiranwj/login_and_registration_api/LoginAndRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.hiranwj.login_and_registration_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiranwj/login_and_registration_api/LoginAndRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiranwj.login_and_registration_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
